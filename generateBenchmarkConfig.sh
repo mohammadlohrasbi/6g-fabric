42748d4d241e543803002f3181b85c3f7bb5bb12
mohammadlohrasbi <46881172+mohammadlohrasbi@users.noreply.github.com>
2025-07-06 03:20:32 -0400

    Update generateBenchmarkConfig.sh

--- generateBenchmarkConfig.sh
+++ generateBenchmarkConfig.sh
@@ -1,180 +1,110 @@
 #!/bin/bash
 
-mkdir -p caliper-benchmarks
+# دایرکتوری برای فایل‌های پیکربندی
+mkdir -p config
 
-cat << EOF > caliper-benchmarks/benchmark.yaml
+# تولید فایل benchmark.yaml
+cat << EOF > config/benchmark.yaml
 test:
-  name: blockchain-performance-test
-  description: Performance test for Hyperledger Fabric chaincodes
+  name: fabric-benchmark
+  description: Benchmark for Hyperledger Fabric with 19 channels and 85 chaincodes
   workers:
+    type: local
     number: 10
   rounds:
 EOF
 
-channels=(
-  "generalchannelapp"
-  "securitychannelapp"
-  "monitoringchannelapp"
-  "iotchannelapp"
-  "channelapp5"
-  "channelapp6"
-  "channelapp7"
-  "channelapp8"
-  "channelapp9"
-  "channelapp10"
-  "channelapp11"
-  "channelapp12"
-  "channelapp13"
-  "channelapp14"
-  "channelapp15"
-  "channelapp16"
-  "channelapp17"
-  "channelapp18"
-  "channelapp19"
-)
-
-chaincodes=(
-  "GeoBasedAllocation"
-  "AntennaLoadBalancer"
-  "GeoPriorityAssign"
-  "DynamicGeoSwitch"
-  "GeoClusterAssign"
-  "GeoSignalStrength"
-  "GeoLatencyOptimizer"
-  "GeoEnergyEfficient"
-  "GeoMultiAntennaAssign"
-  "GeoPathLossAssign"
-  "ResourceAllocate"
-  "BandwidthShare"
-  "DynamicRouting"
-  "LoadBalance"
-  "UserAuth"
-  "ThreatDetect"
-  "NetworkMonitor"
-  "DataAnalytics"
-  "TransactionAudit"
-  "ComplianceAudit"
-  "FaultDetect"
-  "PredictiveMaintenance"
-  "ResourceMonitor"
-  "QoSMonitor"
-  "LatencyTracker"
-  "EnergyOptimizer"
-  "SecurityAudit"
-  "PolicyEnforcer"
-  "DataIntegrity"
-  "AccessControl"
-  "PerformanceMonitor"
-  "NetworkOptimizer"
-  "TrafficManager"
-  "ServiceAllocator"
-  "IoTManager"
-  "DeviceRegistry"
-  "SignalOptimizer"
-  "ChannelManager"
-  "ResourceScheduler"
-  "FaultTolerance"
-  "DataValidator"
-  "NetworkDiagnostics"
-  "UserManager"
-  "AntennaManager"
-  "ClusterManager"
-  "GeoTracker"
-  "LoadDistributor"
-  "PriorityManager"
-  "DynamicAllocator"
-  "EnergyTracker"
-  "SignalMonitor"
-  "LatencyOptimizer"
-  "PathLossCalculator"
-  "RedundancyManager"
-  "FailoverHandler"
-  "ResourceBalancer"
-  "NetworkPlanner"
-  "IoTController"
-  "SecurityEnforcer"
-  "ComplianceTracker"
-  "AuditManager"
-  "PerformanceAnalyzer"
-  "ServiceTracker"
-  "ResourceController"
-  "NetworkManager"
-  "DataManager"
-)
-
-for channel in "${channels[@]}"; do
-  for chaincode in "${chaincodes[@]}"; do
-    cat << EOF >> caliper-benchmarks/benchmark.yaml
-    - txNumber: 1000
+# تولید دورهای تست برای هر کانال
+for channel in generalchannelapp securitychannelapp monitoringchannelapp iotchannelapp {5..19}; do
+  if [[ $channel =~ [0-9] ]]; then
+    channel_name="channelapp${channel}"
+  else
+    channel_name="${channel}"
+  fi
+  # دورهای تست برای قراردادهای جغرافیایی
+  for geo_cc in {1..10}; do
+    cat << EOF >> config/benchmark.yaml
+    - label: ${channel_name}_geo_cc${geo_cc}_invoke
+      txType: invoke
+      chaincodeID: geo_cc${geo_cc}
+      channel: ${channel_name}
+      contractFunction: AssignResource
+      contractArguments: ["resourceID", "amount", "start", "end"]
       rateControl:
         type: fixed-rate
         opts:
           tps: 100
       workload:
-        module: workload.js
-        arguments:
-          chaincodeID: ${chaincode}
-          channel: ${channel}
-          txType: invoke
-          contractFunction: AssignResource
-          contractArguments: ["resource${RANDOM}", "100", "$(date -u +%Y-%m-%dT%H:%M:%SZ)", "$(date -u +%Y-%m-%dT%H:%M:%SZ)"]
-    - txNumber: 500
+        module: workload/callback.js
+    - label: ${channel_name}_geo_cc${geo_cc}_query
+      txType: query
+      chaincodeID: geo_cc${geo_cc}
+      channel: ${channel_name}
+      contractFunction: QueryResource
+      contractArguments: ["resourceID"]
       rateControl:
         type: fixed-rate
         opts:
-          tps: 50
+          tps: 200
       workload:
-        module: workload.js
-        arguments:
-          chaincodeID: ${chaincode}
-          channel: ${channel}
-          txType: query
-          contractFunction: QueryResource
-          contractArguments: ["resource${RANDOM}"]
+        module: workload/callback.js
+EOF
+  done
+  # دورهای تست برای قراردادهای عمومی
+  for public_cc in {1..75}; do
+    cat << EOF >> config/benchmark.yaml
+    - label: ${channel_name}_public_cc${public_cc}_invoke
+      txType: invoke
+      chaincodeID: public_cc${public_cc}
+      channel: ${channel_name}
+      contractFunction: AssignResource
+      contractArguments: ["resourceID", "amount", "start", "end"]
+      rateControl:
+        type: fixed-rate
+        opts:
+          tps: 100
+      workload:
+        module: workload/callback.js
+    - label: ${channel_name}_public_cc${public_cc}_query
+      txType: query
+      chaincodeID: public_cc${public_cc}
+      channel: ${channel_name}
+      contractFunction: QueryResource
+      contractArguments: ["resourceID"]
+      rateControl:
+        type: fixed-rate
+        opts:
+          tps: 200
+      workload:
+        module: workload/callback.js
 EOF
   done
 done
 
-cat << EOF >> caliper-benchmarks/benchmark.yaml
+cat << EOF >> config/benchmark.yaml
 network:
-  config: ../networkConfig.yaml
   fabric:
-    connectionProfile: connection-org1.json
-    walletPath: wallet
+    topology: config/networkConfig.yaml
+    connectionProfile:
+      path: config/connection-org1.json
+      discover: true
+monitor:
+  type:
+    - docker
+  docker:
+    name:
+      - peer0.org1.example.com
+      - peer0.org2.example.com
+      - peer0.org3.example.com
+      - peer0.org4.example.com
+      - peer0.org5.example.com
+      - peer0.org6.example.com
+      - peer0.org7.example.com
+      - peer0.org8.example.com
+      - peer0.org9.example.com
+      - peer0.org10.example.com
+      - orderer.example.com
+  interval: 1
 EOF
 
-cat << EOF > caliper-benchmarks/workload.js
-'use strict';
-
-module.exports.info = 'Custom workload for testing chaincodes';
-
-let bc, contx;
-
-module.exports.init = async function(blockchain, context, args) {
-    bc = blockchain;
-    contx = context;
-    contx.chaincodeID = args.chaincodeID;
-    contx.channel = args.channel;
-    contx.txType = args.txType;
-    contx.contractFunction = args.contractFunction;
-    contx.contractArguments = args.contractArguments;
-};
-
-module.exports.run = async function() {
-    let args = {
-        chaincodeFunction: contx.contractFunction,
-        chaincodeArguments: contx.contractArguments
-    };
-    if (contx.txType === 'invoke') {
-        return bc.invokeSmartContract(contx, contx.chaincodeID, contx.channel, args);
-    } else {
-        return bc.querySmartContract(contx, contx.chaincodeID, contx.channel, args);
-    }
-};
-
-module.exports.end = async function() {
-    return Promise.resolve();
-};
-EOF
-
-echo "Generated Caliper benchmark configuration"
+echo "Generated config/benchmark.yaml"
